753565fe37e13ce48d2b7993fc57a08fa1ac37ae
Josh Kocur <josh.kocur@usask.ca>
2023-04-14 13:44:21 -0600

    refactor(config/settings.js): remove depricated settings

--- config/settings.js
+++
@@ -1,9 +0,0 @@
-/**
- * Currently, reading from `./config.json`
- * 
- * Will look into adding defaults later.
- */
-const fs = require('fs');
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-
-exports.default=config;
