cd63ac5587f560a6d95237a42cadaa05fe73416e
Josh Kocur <josh.kocur@usask.ca>
2023-04-15 10:58:49 -0600

    refactor(db/interface.js): add abc for record validations

--- db/interface.js
+++ db/interface.js
@@ -2,3 +2,29 @@
  * Future home of classes to be used to interface with mysql
  * 
  */
+
+const error = () =>{
+    throw new Error("Table class is abstract!");
+}
+
+class Table {
+    constructor(obj){
+        if(this.constructor == Table){
+            error();
+        }
+        this.required_keys().forEach(item =>{
+            if(!obj.hasOwnProperty(item)){
+                throw new Error(`Missing key(s): ${item}. Required to have all of ${this.required_keys()} for table ${this.name()}`);
+            }
+        })
+        this.record = obj
+    }
+
+    name(){
+        error();
+    }
+
+    required_keys(){
+        error();
+    }
+}
