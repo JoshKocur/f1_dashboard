497810aefb585aa0fa81957c451f10c352aaebfd
Josh Kocur <josh.kocur@usask.ca>
2023-05-08 21:23:57 -0600

    refactor(interface.js): favor flattening table subclasses over one layer deep nesting
    Change CLASS.record.property to CLASS.property to ensure ease of reference

--- db/interface.js
+++ db/interface.js
@@ -15,17 +15,37 @@ class Table {
             if(!obj.hasOwnProperty(item)){
                 throw new TypeError(`Missing key(s): ${item}. Required to have all of ${this.required_keys()} for table ${this.name()}`);
             }
+        });
+        for (var key in obj){
+            this[key] = obj[key];
+        }
+        this.optional_keys().forEach(item =>{
+            if(!this.hasOwnProperty(item)){
+                this[item] = null;
+            }
         })
-        this.record = obj
+        // TODO -- Consider adding a timestamp here. 
     }
 
     name(){
         error();
     }
 
+    /**
+     * Values in this set MUST be present in `this.record`.  If not present `TypeError` will be thrown.
+     */
     required_keys(){
         error();
     }
+
+    /**
+     * Keys here are to be considered optional and will be assigned a `null` type value if not 
+     * present in the object passed into the constructor.
+    */
+    optional_keys(){
+        error();
+    }
+
 }
 
 class RaceControlMessage extends Table{
@@ -62,11 +82,15 @@ class WeatherData extends Table{
             // TimeStamp
         ])
     }
+    optional_keys(){
+        return new Set([
+            "UTC"]);
+    }
 }
 
 class TrackStatus{
     name(){
-        return "TrackStatus"
+        return "TrackStatus";
     }
     required_keys(){
         return new Set([
@@ -74,16 +98,29 @@ class TrackStatus{
             "Status",
             "Message"
             // _kf???
-        ])
+        ]);
     }
 }
 
 
-// export every record in "namespace" for ease of use
+class SessionData{
+    name(){
+        return "SessionData";
+    }
+    required_keys(){
+        "SessionId",
+        "TrackStatus",
+        "StatusSeries"
+        // TimeStamp??? 
+    }
+}
+
+// export every Table in "namespace" for ease of use
 name_space = {
     "WeatherData": WeatherData,
     "RaceControlMessage": RaceControlMessage,
     "TrackStatus": TrackStatus,
+    "SessionData": SessionData,
 };
 
 module.exports=name_space;
