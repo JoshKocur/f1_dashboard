dbb07ba83df9453b60a08d82bb7e9f0616f40a1b
Josh Kocur <josh.kocur@usask.ca>
2023-06-19 17:00:12 -0600

    parse to obj

--- process_file.js
+++ process_file.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const readline = require('readline');
 
+const parser = require('./parser.js');
+
+
 async function processLineByLine(pathToTextFile) {
   const fileStream = fs.createReadStream(pathToTextFile);
 
@@ -10,11 +13,12 @@ async function processLineByLine(pathToTextFile) {
   });
   // Note: we use the crlfDelay option to recognize all instances of CR LF
   // ('\r\n') in input.txt as a single line break.
-
   for await (const line of rl) {
     // Each line in input.txt will be successively available here as `line`.
-    console.log(`Line from file: ${line}`);
+    //console.log(`Line from file: ${line}`);
+    let data = parser.parseData(JSON.parse(line));
+    console.log(data);
   }
 }
 
-processLineByLine();
+processLineByLine('./short_file.txt');
